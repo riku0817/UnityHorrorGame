e636a2de2c68d78aefef5844c918194ae15104ae
rikuto <inbederacade@gmail.com>
2024-10-04 01:05:00 +0900

    ゲーム内からESCキーでタイトル画面に遷移できる機能を追加 ※音量バーは現状ぶっ壊れてる

--- HrrorGameUnity/My project (1)/Assets/Scenes/コードフォルダ/AudioConfig.cs
+++ HrrorGameUnity/My project (1)/Assets/Scenes/コードフォルダ/AudioConfig.cs
@@ -8,15 +8,15 @@ public class AudioConfig : MonoBehaviour
 {
     [SerializeField] AudioMixer audioMixer;
     [SerializeField] AudioSource bgmAudioSource;
-    [SerializeField] Slider bgmSlider;
+    [SerializeField] Slider volumeSlider;
     private void Start()
     {
-        bgmSlider.onValueChanged.AddListener((value)=>
+        volumeSlider.onValueChanged.AddListener((value)=>
         {
             value = Mathf.Clamp01(value);
             float decibel = 20f * Mathf.Log10(value);
             decibel = Mathf.Clamp(decibel,-80f,0f);
-            audioMixer.SetFloat("BGM",decibel);
+            audioMixer.SetFloat("Master",decibel);
         });
     }
 }
--- HrrorGameUnity/My project (1)/Assets/Scenes/コードフォルダ/GameManager.cs
+++ HrrorGameUnity/My project (1)/Assets/Scenes/コードフォルダ/GameManager.cs
@@ -5,17 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    bool optionbool = false;
     [SerializeField] GameObject Option;
     public void StartButton()
     {
         SceneManager.LoadScene("HospitalScene");
     }
+
+    public void ReturnMenuButton()
+    {
+        SceneManager.LoadScene("MenuScene");
+        Option.SetActive(false);
+        optionbool = false;
+    }
+        public void GameExitButton()
+    {
+       Application.Quit();
+    }
     public void ShowOption()
     {
         Option.SetActive(true);
+        optionbool = true;
     }
-      public void HideOption()
+    public void HideOption()
     {
         Option.SetActive(false);
+        optionbool = false;
+    }
+
+    //ESCオプション画面表示
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (optionbool == false)
+            {
+                Option.SetActive(true);
+                optionbool = true;
+            }
+            else if (optionbool == true)
+            {
+                Option.SetActive(false);
+                optionbool = false;
+            }
+
+
+        }
     }
 }
