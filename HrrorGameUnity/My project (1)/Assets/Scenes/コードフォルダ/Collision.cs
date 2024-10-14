14fab28086c9fc66b7cf8ba2bf660792be36e24e
rikuto <inbederacade@gmail.com>
2024-10-15 01:43:56 +0900

    RayCastがマジ分からんすぎたから物理的な方式をテスト（Colliderを使ったインタラクト法）
    カーソル中央固定でも調べられるようになった

--- HrrorGameUnity/My project (1)/Assets/Scenes/コードフォルダ/Collision.cs
+++ HrrorGameUnity/My project (1)/Assets/Scenes/コードフォルダ/Collision.cs
@@ -3,12 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MyScript : MonoBehaviour {
-    [SerializeField] GameObject Chair;
-  // 当たった時に呼ばれる関数
-    void OnCollisionEnter(Collision collision)
+public class MyScript : MonoBehaviour
+{
+  [SerializeField] GameObject Object;
+  [SerializeField] GameObject clicktext;
+  [SerializeField] GameObject message;
+  // 当たっている時に呼ばれる関数
+  void OnCollisionStay(Collision collision)
+  {
+    if (collision.gameObject.name == "FPSController")
     {
-        Debug.Log("Hit"); // ログを表示する
-        Chair.layer=3;
+      clicktext.SetActive(true);
+      Debug.Log(collision.gameObject.name+"と衝突した"); // ログを表示する
+      Object.SetLayer(3);
+      if (Input.GetMouseButton(0))
+      {
+        Debug.Log("Clicked");
+        message.SetActive(true);
+      }
     }
+
+  }
+
+  //オブジェクトが離れた時
+  void OnCollisionExit(Collision collision)
+  {
+    clicktext.SetActive(false);
+    Debug.Log("Leave"); // ログを表示する
+    Object.SetLayer(0);
+  }
+}
+
+/// <summary>
+/// GameObjectの拡張クラス
+/// </summary>
+public static class GameObjectExtension
+{
+
+  /// <summary>
+  /// レイヤーを設定する
+  /// </summary>
+  /// <param name="needSetChildrens">子にもレイヤー設定を行うか</param>
+  public static void SetLayer(this GameObject gameObject, int layerNo, bool needSetChildrens = true)
+  {
+    if (gameObject == null)
+    {
+      return;
+    }
+    gameObject.layer = layerNo;
+
+    //子に設定する必要がない場合はここで終了
+    if (!needSetChildrens)
+    {
+      return;
+    }
+
+    //子のレイヤーにも設定する
+    foreach (Transform childTransform in gameObject.transform)
+    {
+      SetLayer(childTransform.gameObject, layerNo, needSetChildrens);
+    }
+  }
+
+  /// <summary>
+  /// マテリアル設定
+  /// </summary>
+  /// <param name="needSetChildrens">子にもマテリアル設定を行うか</param>
+  public static void SetMaterial(this GameObject gameObject, Material setMaterial, bool needSetChildrens = true)
+  {
+    if (gameObject == null)
+    {
+      return;
+    }
+
+    //レンダラーがあればそのマテリアルを変更
+    if (gameObject.GetComponent<Renderer>())
+    {
+      gameObject.GetComponent<Renderer>().material = setMaterial;
+    }
+
+    //子に設定する必要がない場合はここで終了
+    if (!needSetChildrens)
+    {
+      return;
+    }
+
+    //子のマテリアルにも設定する
+    foreach (Transform childTransform in gameObject.transform)
+    {
+      SetMaterial(childTransform.gameObject, setMaterial, needSetChildrens);
+    }
+
+  }
+
+
 }
---
+++ HrrorGameUnity/My project (1)/Assets/Scenes/コードフォルダ/UseDoor.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseDoor : MonoBehaviour
+{
+    [SerializeField] GameObject Object;
+    [SerializeField] GameObject clicktext;
+    Animation anime;
+    DoorAnimation dooranimationscript;
+    public bool animationbool = true;
+    // 当たっている時に呼ばれる関数
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name == "FPSController")
+        {
+            anime = this.gameObject.GetComponent<Animation>();
+            clicktext.SetActive(true);
+            Debug.Log(collision.gameObject.name + "と衝突した"); // ログを表示する
+            Object.SetLayer(3);
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("Clicked");
+                // 暫定策：アニメーションを一回だけ再生
+                if (animationbool == true)
+                {
+                    anime.Play();
+                    animationbool = false;
+                }
+            }
+
+        }
+
+    }
+
+    //オブジェクトが離れた時
+    void OnCollisionExit(Collision collision)
+    {
+        clicktext.SetActive(false);
+        Debug.Log("Leave"); // ログを表示する
+        Object.SetLayer(0);
+    }
+}
